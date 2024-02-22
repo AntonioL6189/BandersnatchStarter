6a910e39e3ff0352d841322aa1dc94a6560e924b
Antonio Legrier <112994256+AntonioL6189@users.noreply.github.com>
2024-02-22 11:33:59 -0500

    commit

--- app/data.py
+++ app/data.py
@@ -1,25 +1,40 @@
 from os import getenv
-
-from certifi import where
-from dotenv import load_dotenv
 from MonsterLab import Monster
 from pandas import DataFrame
 from pymongo import MongoClient
+from dotenv import load_dotenv
+from certifi import where
 
 
 class Database:
+    load_dotenv()
+    database = MongoClient(getenv("DB_URL"), tlsCAFile=where())["Database"]
 
-    def seed(self, amount):
-        pass
+    def __init__(self, collection: str):
+        self.collection = self.database[collection]
 
-    def reset(self):
-        pass
+    def seed(self, amount=1000) -> bool:
+        records = [Monster().to_dict() for i in range(amount)]
+        return self.collection.insert_many(records).acknowledged
+
+    def reset(self) -> bool:
+        return self.collection.delete_many(filter={}).acknowledged
 
     def count(self) -> int:
-        pass
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        return DataFrame(list(self.collection.find({}, {"_id": False})))
 
     def html_table(self) -> str:
-        pass
+        count = self.count()
+        if count > 0:
+            return self.dataframe().to_html()
+        else:
+            return "None"
+
+
+if __name__ == '__main__':
+    db = Database("Collection")
+    db.seed()
+    print("Collections created:", db.count())
