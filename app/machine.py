0f457260513db0b37b45f730917b39e146d184d3
Antonio Legrier <112994256+AntonioL6189@users.noreply.github.com>
2024-03-14 22:10:40 -0400

    Come back to edit

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,33 @@
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from pymongo import MongoClient
+
+
 class Machine:
+    def __init__(self, collection: str):
+        client = MongoClient('localhost', 27017)
+        self.database = client['Database']
+        self.collection = self.database[collection]
+        self.model = RandomForestClassifier()
 
-    def __init__(self, df):
-        pass
+    def __call__(self, feature_data):
+        predictions = self.model.predict(feature_data)
+        return predictions
 
-    def __call__(self, feature_basis):
-        pass
+    def train(self, x_train, y_train):
+        self.model.fit(x_train, y_train)
+
+    def predict(self, feature_data):
+        predictions = self.model.predict(feature_data)
+        return predictions
 
     def save(self, filepath):
-        pass
+        joblib.dump(self.model, filepath)
 
     @staticmethod
-    def open(filepath):
-        pass
+    def load(filepath):
+        return joblib.load(filepath)
 
     def info(self):
-        pass
+        print("Model Information:")
+        print(self.model)
